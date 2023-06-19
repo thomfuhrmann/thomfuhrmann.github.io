0fed4835988c0ef690665cf041c70bc81077f454
Thomas Fuhrmann <thomas.fuhrmann@geologie.ac.at>
2023-06-19 10:01:55 +0200

    Add layerlist widget

---
+++ exb_widgets/1.11/layerlist-widget/dist/runtime/translations/de-at.js
@@ -0,0 +1,1 @@
+System.register([],(function(e){return{execute:function(){e({_widgetLabel:"Layerliste",decreaseOpacity:"Deckkraft verringern",increaseOpacity:"Deckkraft erhöhen"})}}}));
---
+++ exb_widgets/1.11/layerlist-widget/dist/runtime/translations/default.ts
@@ -0,0 +1,5 @@
+export default {
+  _widgetLabel: 'Layerlist',
+  decreaseOpacity: 'Decrease opacity',
+  increaseOpacity: 'Increase opacity',
+};
