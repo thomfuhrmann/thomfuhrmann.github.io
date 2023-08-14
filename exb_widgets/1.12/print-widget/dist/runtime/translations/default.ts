469fcd00f3812552c083f8cfe7be7a0ea1e1bb62
Thomas Fuhrmann <thomas.fuhrmann@geologie.ac.at>
2023-08-14 14:27:09 +0200

    Add new print widget

---
+++ exb_widgets/1.12/print-widget/dist/runtime/translations/default.ts
@@ -0,0 +1,23 @@
+export default {
+  _widgetLabel: 'Print',
+  heading: 'PDF drucken',
+  titleLabel: 'Titel',
+  scaleLabel: 'Maßstab',
+  formatLabel: 'Format',
+  button: {
+    print: 'Drucken',
+    cancel: 'Abbrechen',
+  },
+  formats: {
+    'A4 Hochformat': 'A4 Hochformat',
+    'A4 Hochformat mit Legende': 'A4 Hochformat mit Legende',
+    'A4 Querformat': 'A4 Querformat',
+    'A4 Querformat mit Legende': 'A4 Querformat mit Legende',
+    'A3 Hochformat': 'A3 Hochformat',
+    'A3 Hochformat mit Legende': 'A3 Hochformat mit Legende',
+    'A3 Querformat': 'A3 Querformat',
+    'A3 Querformat mit Legende': 'A3 Querformat mit Legende',
+  },
+  buttonPrint: 'Drucken',
+  buttonCancel: 'Abbrechen',
+};
